04ab9642414ec97670850ebd2a53c5ab87901951
yamon <ngockien16@gmail.com>
2016-08-08 11:54:52 +0700

    update db seed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       redirect_to categories_path
     else
       # If user's login doesn't work, send them back to the login form.
-      redirect_to login_path
+      redirect_to login_path,:flash => { :error => "Username or Password invalid" }
     end
   end
 
